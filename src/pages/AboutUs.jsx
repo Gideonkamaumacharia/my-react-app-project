8fb92c1943b35bbafd91561fb6033933b93aa0b9
mary <Mariashee001@gmail.com>
2024-07-17 21:14:43 +0300

    About us

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -4,7 +4,8 @@ function AboutUs() {
   return (
     <div>
       <h2>About Us</h2>
-      <p>We provide a comprehensive survey management system to help you create, manage, and analyze surveys efficiently.</p>
+      <p>We provide a comprehensive survey management system to help you create, manage, and analyze surveys efficiently.
+      facilitate the efficient and effective management of surveys from planning and design to data collection, analysis, and reporting.</p>
       <h2>Who We Are</h2>
       <p>We are a dedicated team of developers, data analysts, and UX designers who believe in the power of feedback. Our journey started with a simple goal: to create a tool that simplifies the survey process and makes data collection accessible to everyone. Whether you're a business looking to understand your customer base, a researcher gathering data for a study, or an educator seeking student feedback, our platform is designed to meet your needs.</p>
       <h2>Our Vision</h2>
